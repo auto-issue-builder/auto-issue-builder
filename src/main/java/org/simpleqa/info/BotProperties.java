8c9e9c0dbc6cfd0ea88cc2b204a265a2269ceb4e
nswon <swnam0729@gmail.com>
2023-11-05 22:20:59 +0900

    fix: bot account ignore

--- src/main/java/org/simpleqa/info/BotProperties.java
+++
@@ -1,6 +0,0 @@
-package org.simpleqa.info;
-
-public class BotProperties {
-    public static final String BOT_EMAIL = "issuebot07@gmail.com";
-    public static final String BOT_PASSWORD = "nswon0729@";
-}
